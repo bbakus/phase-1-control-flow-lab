b17a0e4672a3805f28081ff5a6d4b7ea676543ca
Brandon Bakus <brandonbakus@gmail.com>
2025-01-04 14:30:32 -0500

    Good lab

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+function scuberGreetingForFeet(thisRide){
+    if(thisRide > 2500){
+      return 'No can do.'
+    } 
+    else if (thisRide > 2000){
+      return 'I will gladly take your thirty bucks.'
+    } 
+    else if (thisRide > 400){
+      return 'That will be twenty bucks.'
+    } 
+    else {
+      return 'This one is on me!'
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+scuberGreetingForFeet(199)
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+ternaryCheckCity('NYC')
+ternaryCheckCity('Pittsburgh')
+
+
+function switchOnCharmFromTip(tip){
+      switch(tip){
+        case 'generous':
+          return 'Thank you so much.'
+        case 'not as generous':
+          return 'Thank you.'
+        case 'thanks for everything':
+          return 'Bye.'
+      }
+  }
+
+  switchOnCharmFromTip('generous')
+  switchOnCharmFromTip('not as generous')
+  switchOnCharmFromTip('thanks for everything')
